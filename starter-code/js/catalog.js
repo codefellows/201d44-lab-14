122aadf43fb115b5c4452187f5d0848fdf87ea0b
amjcurtis <curtis.andrewm@gmail.com>
2019-01-24 15:51:30 -0800

    Add initial progress on busmall order site

--- starter-code/js/catalog.js
+++ starter-code/js/catalog.js
@@ -12,9 +12,10 @@ function populateForm() {
   //TODO: Add an <option> tag inside the form's select for each product
   var selectElement = document.getElementById('items');
   for (var i in Product.allProducts) {
-
+    var optEl = document.createElement('option');
+    optEl.textContent = Product.allProducts[i].name;
+    selectElement.appendChild(optEl);
   }
-
 }
 
 // When someone submits the form, we need to add the selected item to the cart
@@ -23,6 +24,7 @@ function populateForm() {
 function handleSubmit(event) {
 
   // TODO: Prevent the page from reloading
+  event.preventDefault();
 
   // Do all the things ...
   addSelectedItemToCart();
@@ -35,12 +37,18 @@ function handleSubmit(event) {
 // TODO: Add the selected item and quantity to the cart
 function addSelectedItemToCart() {
   // TODO: suss out the item picked from the select list
+  var grabElement = document.getElementById('items');
+  var itemPicked = grabElement.options[grabElement.selectedIndex].value;
   // TODO: get the quantity
+  var numberOfItemsPicked = document.getElementById('catalog').elements['quantity'].value;
   // TODO: using those, add one item to the Cart
+  cart.addItem(itemPicked,numberOfItemsPicked);
 }
 
 // TODO: Update the cart count in the header nav with the number of items in the Cart
-function updateCounter() {}
+function updateCounter() {
+
+}
 
 // TODO: As you add items into the cart, show them (item & quantity) in the cart preview div
 function updateCartPreview() {
